b79872f7a3e868651b331fc32e94ad28b5637bf2
Bruno Bulic <brbulic@codebase.hr>
2023-08-08 10:58:45 +0200

    connected to DB

--- babinje/babinje_item.py
+++ babinje/babinje_item.py
@@ -1,8 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = Column(Integer, primary_key=True)
+    email = Column(String(1024), nullable=False, unique=True)
+    reset_string = Column(String(48), nullable=True)
+    operation = Column(String(10), nullable=True, unique=False)    
+    item = db.relationship("BabinjeItem", backref="owning", uselist=False)    
+
 class BabinjeItem(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    item = db.Column(db.String(255))
-    desc = db.Column(db.String(10000))
+    id = Column(Integer, primary_key=True)
+    item = Column(String(255), nullable=False)
+    desc = Column(String(10000), nullable=True)
+    assigned_on = Column(DateTime, nullable=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=True)
--- babinje/hello_world.py
+++ babinje/hello_world.py
@@ -1,5 +1,21 @@
 from flask_restful import Resource
+from .babinje_item import *
 
 class HelloWorld(Resource):
     def get(self):
+
+        i = BabinjeItem(item = "Kolica za automobile", desc="https://www.babycenter.hr/maxi-cosi-autosjedalica-i-size-40-75-cm-pebble-pro-i-size-essenblack-880240.html")
+        u = User(email = "brbulic@gmail.com")
+
+        db.session.add(i)
+        db.session.add(u)
+        db.session.commit()
+
+        last_item = BabinjeItem.query.all()[-1]
+        last_user = User.query.all()[-1]
+
+        last_user.item = last_item
+
+        db.session.commit()
+
         return {"data" : "Hello World"}
